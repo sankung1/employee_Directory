34668e719007cf21e8bf780d9380329422c7d570
Sankung Sillah <ssillah306@gmail.com>
2021-01-05 17:48:13 -0500

    added an emloyee card file

---
+++ employee_directory/src/components/ECard.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {Card} from "react-bootstrap";
+
+function ECard(props){
+    return (
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={props.picture} />
+        <Card.Body>
+          <Card.Title>{props.firstName}</Card.Title>
+          <Card.Title>{props.lastName}</Card.Title>
+          <Card.Title>{props.email}</Card.Title>
+          <Card.Title>{props.phone}</Card.Title>
+        </Card.Body>
+      </Card>
+    );
+}
+
+export default ECard;
